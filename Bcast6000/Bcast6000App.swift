cdda421d279cbd0a8c74fd9d5301d837b2b36183
Douglas Adams <douglas.adams@me.com>
2023-06-08 22:24:22 -0400

    Initial Commit

---
+++ Bcast6000/Bcast6000App.swift
@@ -0,0 +1,17 @@
+//
+//  Bcast6000App.swift
+//  Bcast6000
+//
+//  Created by Douglas Adams on 6/8/23.
+//
+
+import SwiftUI
+
+@main
+struct Bcast6000App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
